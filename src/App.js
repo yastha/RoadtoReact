0cdd41493a2bb7c1cd3bca5b00b5207b7b01aeaa
Astha <yastha.as@gmail.com>
2019-11-20 14:43:51 +0545

    First Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state ={
+    multilpier : [1,2,3,4,5,6,7,8,9,10],
+    multiNumber : 1,
+    flag : false,
+    showButton : false
+}
+handleNumber =(event) =>{
+  console.log("Type Bhayo hai", event.target.value);
+  this.setState({ 
+    multiNumber : event.target.value,
+    flag : false,
+    showButton : true
+    
+  })
 }
 
+  submitHandling =(event) =>{
+    this.setState({
+     flag: true
+
+    })
+  }
+
+render() {
+
+return (
+<div className="App">
+  <label>Enter a number</label>
+ <input name ="multilpier" type = "number" onChange ={this.handleNumber}/>
+ {this.state.showButton && <button type = "submit" onClick = {this.submitHandling}>Generate</button>  }
+ <p>
+   {this.state.flag && 
+    (this.state.multilpier.map((eachNumber, index) => (
+   <li>{eachNumber} * {this.state.multiNumber} = {eachNumber * this.state.multiNumber}</li>
+)) )
+   }
+
+</p>
+ 
+</div>
+);
+}
+}
 export default App;
